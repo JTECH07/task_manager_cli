12b0c7b1a7c4622b35f254c8788d996c31c6daaf
Joseph Alaye <jamstech07@gmail.com>
2026-08-17 18:15:38 +0200

    Initialisation and creation of my important files to use..

---
+++ bin/task_manager_cli.dart
@@ -0,0 +1,5 @@
+import 'package:task_manager_cli/task_manager_cli.dart' as task_manager_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${task_manager_cli.calculate()}!');
+}
---
+++ test/task_manager_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_manager_cli/task_manager_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
